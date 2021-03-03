0ab8fead0b54da34aa4b37f37336938976e040b8
MW Kim <vomin0107@gmail.com>
2021-03-03 13:50:12 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,61 @@
+import random
+import os
+import shutil
+import argparse
+
+dir_path = r'C:\Users\hong0\PycharmProjects\image-scrapping\images'
+
+def createFolder(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        print('Error: Creating directory. ' + directory)
+
+
+def divide_set(file_names, ratio):
+    file_names_len = len(file_names)
+    test_file_names_len = int(file_names_len * ratio)
+
+    for i in range(test_file_names_len):
+        file_name = file_names[i]
+        print(file_name)
+        jpg_name = file_name + '.jpg'
+        xml_name = file_name + '.xml'
+        shutil.copyfile(dir_path + '/' + jpg_name, './test/' + jpg_name)
+        shutil.copyfile(dir_path + '/' + xml_name, './test/' + xml_name)
+
+    for i in range(test_file_names_len, file_names_len):
+        file_name = file_names[i]
+        print(file_name)
+        jpg_name = file_name + '.jpg'
+        xml_name = file_name + '.xml'
+        shutil.copyfile(dir_path + '/' + jpg_name, './train/' + jpg_name)
+        shutil.copyfile(dir_path + '/' + xml_name, './train/' + xml_name)
+
+
+def main():
+    os_files = os.listdir(dir_path)
+    file_names = []
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ratio', help='.ratio of test set',
+                        default=0.2)
+    args = parser.parse_args()
+
+    for dir in os_files:
+        if dir[:-4] not in file_names:
+            file_names.append(dir[:-4])
+
+    random.shuffle(file_names)
+
+    createFolder('./test')
+    createFolder('./train')
+
+    divide_set(file_names, args.ratio)
+
+    print('done.')
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
